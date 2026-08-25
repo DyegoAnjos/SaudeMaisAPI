8f7f724c989f64f886bb3df69e31cb2b1d9f497e
Dyego dos Anjos Cordeiro <107328787+DyegoAnjos@users.noreply.github.com>
2026-08-25 08:51:04 -0300

    teste

--- saudemaisapi/database.py
+++ saudemaisapi/database.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.asyncio import (
 
 from saudemaisapi.settings import Settings
 
-engine = create_engine(Settings().DATABASE_URL)
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
 async def get_db():
-    async with Session(engine, expire_on_commit=False) as session:
+    async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
--- saudemaisapi/routers/unidades_saude.py
+++ saudemaisapi/routers/unidades_saude.py
@@ -1,108 +1,108 @@
-from http import HTTPStatus
-from typing import Annotated
-
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
-
-from saudemaisapi.database import get_db
-from saudemaisapi.models import Unidade_saude
-from saudemaisapi.schemas import (
-    Filtro_Paginas,
-    MessageSchema,
-    Unidade_saude_retorno_Schema,
-    Unidade_saude_Schema,
-    Unidades_saude_lista_Schema,
-)
-
-router = APIRouter(prefix='/unidades-saude', tags=['Unidades de saúde'])
-SessionDep = Annotated[Session, Depends(get_db)]
-
-
-@router.get('/', response_model=Unidades_saude_lista_Schema)
-def listar_unidades(
-    session: SessionDep,
-    filtro: Annotated[Filtro_Paginas, Query()],
-):
-    unidades = session.scalars(
-        select(Unidade_saude).limit(filtro.limit).offset(filtro.offset)
-    ).all()
-    return {'unidades': unidades}
-
-
-@router.get('/{id_unidade}', response_model=Unidade_saude_retorno_Schema)
-def buscar_unidade(id_unidade: int, session: SessionDep):
-    unidade = session.get(Unidade_saude, id_unidade)
-    if not unidade:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='Unidade de saúde não encontrada',
-        )
-    return unidade
-
-
-@router.post(
-    '/',
-    status_code=HTTPStatus.CREATED,
-    response_model=Unidade_saude_retorno_Schema,
-)
-def criar_unidade(dados: Unidade_saude_Schema, session: SessionDep):
-    unidade = Unidade_saude(**dados.model_dump())
-    session.add(unidade)
-    try:
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='Unidade de saúde já existe',
-        )
-    session.refresh(unidade)
-    return unidade
-
-
-@router.put('/{id_unidade}', response_model=Unidade_saude_retorno_Schema)
-def atualizar_unidade(
-    id_unidade: int,
-    dados: Unidade_saude_Schema,
-    session: SessionDep,
-):
-    unidade = session.get(Unidade_saude, id_unidade)
-    if not unidade:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='Unidade de saúde não encontrada',
-        )
-    for campo, valor in dados.model_dump().items():
-        setattr(unidade, campo, valor)
-    try:
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='Unidade de saúde já existe',
-        )
-    session.refresh(unidade)
-    return unidade
-
-
-@router.delete('/{id_unidade}', response_model=MessageSchema)
-def remover_unidade(id_unidade: int, session: SessionDep):
-    unidade = session.get(Unidade_saude, id_unidade)
-    if not unidade:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='Unidade de saúde não encontrada',
-        )
-    session.delete(unidade)
-    try:
-        session.commit()
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='Unidade de saúde está sendo utilizada',
-        )
-    return {'mensagem': 'Unidade de saúde removida com sucesso!'}
+# from http import HTTPStatus
+# from typing import Annotated
+#
+# from fastapi import APIRouter, Depends, HTTPException, Query
+# from sqlalchemy import select
+# from sqlalchemy.exc import IntegrityError
+# from sqlalchemy.orm import Session
+#
+# from saudemaisapi.database import get_db
+# from saudemaisapi.models import Unidade_saude
+# from saudemaisapi.schemas import (
+#     Filtro_Paginas,
+#     MessageSchema,
+#     Unidade_saude_retorno_Schema,
+#     Unidade_saude_Schema,
+#     Unidades_saude_lista_Schema,
+# )
+#
+# router = APIRouter(prefix='/unidades-saude', tags=['Unidades de saúde'])
+# SessionDep = Annotated[Session, Depends(get_db)]
+#
+#
+# @router.get('/', response_model=Unidades_saude_lista_Schema)
+# def listar_unidades(
+#     session: SessionDep,
+#     filtro: Annotated[Filtro_Paginas, Query()],
+# ):
+#     unidades = session.scalars(
+#         select(Unidade_saude).limit(filtro.limit).offset(filtro.offset)
+#     ).all()
+#     return {'unidades': unidades}
+#
+#
+# @router.get('/{id_unidade}', response_model=Unidade_saude_retorno_Schema)
+# def buscar_unidade(id_unidade: int, session: SessionDep):
+#     unidade = session.get(Unidade_saude, id_unidade)
+#     if not unidade:
+#         raise HTTPException(
+#             status_code=HTTPStatus.NOT_FOUND,
+#             detail='Unidade de saúde não encontrada',
+#         )
+#     return unidade
+#
+#
+# @router.post(
+#     '/',
+#     status_code=HTTPStatus.CREATED,
+#     response_model=Unidade_saude_retorno_Schema,
+# )
+# def criar_unidade(dados: Unidade_saude_Schema, session: SessionDep):
+#     unidade = Unidade_saude(**dados.model_dump())
+#     session.add(unidade)
+#     try:
+#         session.commit()
+#     except IntegrityError:
+#         session.rollback()
+#         raise HTTPException(
+#             status_code=HTTPStatus.CONFLICT,
+#             detail='Unidade de saúde já existe',
+#         )
+#     session.refresh(unidade)
+#     return unidade
+#
+#
+# @router.put('/{id_unidade}', response_model=Unidade_saude_retorno_Schema)
+# def atualizar_unidade(
+#     id_unidade: int,
+#     dados: Unidade_saude_Schema,
+#     session: SessionDep,
+# ):
+#     unidade = session.get(Unidade_saude, id_unidade)
+#     if not unidade:
+#         raise HTTPException(
+#             status_code=HTTPStatus.NOT_FOUND,
+#             detail='Unidade de saúde não encontrada',
+#         )
+#     for campo, valor in dados.model_dump().items():
+#         setattr(unidade, campo, valor)
+#     try:
+#         session.commit()
+#     except IntegrityError:
+#         session.rollback()
+#         raise HTTPException(
+#             status_code=HTTPStatus.CONFLICT,
+#             detail='Unidade de saúde já existe',
+#         )
+#     session.refresh(unidade)
+#     return unidade
+#
+#
+# @router.delete('/{id_unidade}', response_model=MessageSchema)
+# def remover_unidade(id_unidade: int, session: SessionDep):
+#     unidade = session.get(Unidade_saude, id_unidade)
+#     if not unidade:
+#         raise HTTPException(
+#             status_code=HTTPStatus.NOT_FOUND,
+#             detail='Unidade de saúde não encontrada',
+#         )
+#     session.delete(unidade)
+#     try:
+#         session.commit()
+#     except IntegrityError:
+#         session.rollback()
+#         raise HTTPException(
+#             status_code=HTTPStatus.CONFLICT,
+#             detail='Unidade de saúde está sendo utilizada',
+#         )
+#     return {'mensagem': 'Unidade de saúde removida com sucesso!'}
